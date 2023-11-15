d059394e59071fb8fb68d2c8b8dc6a21e6324fab
EC2 Default User <ec2-user@ip-172-31-2-143.ap-southeast-1.compute.internal>
2023-11-15 06:35:37 +0000

    empty pipeline

---
+++ lib/cdk-pipeline-stack.ts
@@ -0,0 +1,31 @@
+ import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+    import { Construct } from 'constructs';
+    import {  Stack, StackProps } from 'aws-cdk-lib';
+
+    /**
+    * The stack that defines the application pipeline
+    */
+    export class CdkPipelineStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+        // The pipeline name
+        pipelineName: 'MyServicePipeline',
+
+        // How it will be built and synthesized
+        synth: new ShellStep('Synth', {
+            // Where the source can be found
+            input: CodePipelineSource.gitHub('TonyChanhNguyen/FCJ-CICD-CDK-Beanstalk-Workshop', 'main'),   
+            // Build and run cdk synth
+            commands: [
+            'npm ci',
+            'npm run build',
+            'npx cdk synth'
+            ],
+        }),
+        });
+
+        // This is where we add the application stages
+    }
+    }
